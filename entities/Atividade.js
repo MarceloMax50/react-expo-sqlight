3b594fce24e478630934b685676136d1a833aa37
MarceloMax50 <maxmelo93@gmail.com>
2022-03-18 22:11:35 -0300

    atualiza

--- entities/Atividade.js
+++ entities/Atividade.js
@@ -1,3 +1,13 @@
 export class Atividade {
-
+    constructor(id, descricao, tipo, local, dataEntrega, horaEntrega, status) {
+        this.id = id;
+        this.descricao = descricao;
+        this.tipo = tipo;
+        this.local = local;
+        this.dataEntrega = dataEntrega;
+        this.horaEntrega = horaEntrega;
+        this.status = status;
+    };
 }
+
+export default Atividade;
--- ui/Home/index.js
+++ ui/Home/index.js
@@ -4,12 +4,22 @@ import {
 } from 'react-native';
 import { Ionicons, Entypo, AntDesign } from '@expo/vector-icons';
 import styles from '../styles';
+import Atividade from '../../entities/Atividade'
+import { useState, useEffect } from 'react';
 
 export default function Home({ navigation }) {
+
+    let atividade = new Atividade();
+    const [atividades, setAtividades] = useState([]);
+
+    setAtividades([
+        atividade
+    ]);
+
     return (
         <View style={styles.container}>
 
-            <Text style={styles.titulo}>Inicial</Text>
+            <Text style={styles.container}>Inicial</Text>
 
             <TouchableOpacity style={styles.botao}
                 onPress={() => navigation.navigate('CadastroAtividade')}>
@@ -20,26 +30,24 @@ export default function Home({ navigation }) {
                 onPress={() => navigation.navigate('CadastroTipoAtividade')}>
                 <Text>Cadastro de tipo de atividade</Text>
             </TouchableOpacity>
-
             <ScrollView style={styles.listaContatos}>
                 {
-                    contacts.map((contact, index) => (
+                    atividades.map((atividade, index) => (
                         <View style={styles.contato} key={index.toString()}>
 
                             <View style={styles.dadosListaTelefone}>
-                                <Text style={styles.listaNome}> {contact.name}</Text>
+                                <Text style={styles.listaNome}> {atividade.name}</Text>
                             </View>
                             <View style={styles.dadosEmail}>
-                                <Image source={iconMail} style={styles.iconTelefone} />
-                                <Text style={styles.listaTelefone} >{contact.email} </Text>
+                                <Text style={styles.listaTelefone} >{atividade.email} </Text>
                             </View>
 
                             <View style={styles.dadosBotoesAcao}>
-                                <TouchableOpacity onPress={() => shallDeleteOne(contact.code)}>
+                                <TouchableOpacity onPress={() => shallDeleteOne(atividade.code)}>
                                     <Ionicons name="md-remove-circle" size={32} color="red" />
                                 </TouchableOpacity>
 
-                                <TouchableOpacity onPress={() => editar(contact.code)}>
+                                <TouchableOpacity onPress={() => editar(atividade.code)}>
                                     <AntDesign name="edit" size={32} color="black" />
                                 </TouchableOpacity>
 
